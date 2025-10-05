87081b1c31040eec4afbade7e1cad42d78dd275a
Terence Pae <terencepae@gmail.com>
2025-10-05 07:23:25 -0700

    Initial Commit

---
+++ tweaks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tweaks
+//
+//  Created by Terence on 10/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tweaks/tweaksApp.swift
@@ -0,0 +1,17 @@
+//
+//  tweaksApp.swift
+//  tweaks
+//
+//  Created by Terence on 10/5/25.
+//
+
+import SwiftUI
+
+@main
+struct tweaksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
